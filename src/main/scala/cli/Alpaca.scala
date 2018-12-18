2413bff7b526480f0a4b0c97ea04fde8347bc77f
asalazar <a.n.salazar@tudelft.nl>
2018-12-18 17:51:50 +0100

    Improved main cli

--- src/main/scala/cli/Alpaca.scala
+++ src/main/scala/cli/Alpaca.scala
@@ -15,10 +15,14 @@ object Alpaca {
         "build-db                Kmerize binned-subregions for a subject genome and store as a database.\n" +
         "db-metrics              Obtain metrics for a given database.\n\n" +
         "TARGET COMPARISON\n" +
-        "genome-similarity       Compute similarity between a target genome and given alpaca database.\n\n" +
+        "genome-similarity       Compute similarity between a target genome and given alpaca database.\n" +
+        "batch-submit            Automated scripts for multiple target genome comparisons.\n\n"+
         "POPULATION COMPARISON\n" +
         "population-summary      Summarize a set of target genome similarities as a population.\n" +
         "population-metrics      Obtain metrics for a summarized population.\n\n" +
+        "VISUALIZATION\n" +
+        "alpaca-layout           Generate an alpaca layout figure.\n" +
+        "tree-tracing            Generate a tree tracing figure.\n\n" +
         "MISC.\n"+
         "mash-sketches           Automated script for constructing mash sketches.\n"+
         "mash-distances          Automated script for computing paiwise mash distance.\n"
@@ -36,6 +40,8 @@ object Alpaca {
         case "population-metrics" => genome_comparison.PopulationMetrics.main(args.drop(1))
         case "mash-sketches" => mash.ConstructMashSketches.main(args.drop(1))
         case "mash-distances" => mash.ConstructMashDistance.main(args.drop(1))
+        case "tree-tracing" => visualization.TreeTracing.main(args.drop(1))
+        case "alpaca-layout" => visualization.AlpacaLayout.main(args.drop(1))
         case _ => println(help)
       }
     }
